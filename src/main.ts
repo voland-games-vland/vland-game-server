a58eb2f9c9bb644a1b3abd71b1bb8ceb5151bc0d
Wenish <Wenish@users.noreply.github.com>
2023-01-14 00:37:40 +0100

    update basic auth

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config'
 import express from 'express'
-// import basicAuth from 'express-basic-auth'
+import basicAuth from 'express-basic-auth'
 import { monitor } from '@colyseus/monitor'
 import { Server } from "colyseus"
 import { WebSocketTransport } from '@colyseus/ws-transport'
@@ -10,16 +10,14 @@ import config from './config'
 const bootstrap = async () => {
     const app = express()
 
-    /*
     const basicAuthMiddleware = basicAuth({
         users: {
             [config.colyseus.monitor.auth.username]: config.colyseus.monitor.auth.password,
         },
         challenge: true,
     })
-    */
 
-    app.use('/colyseus', monitor())
+    app.use('/colyseus', basicAuthMiddleware, monitor())
     app.get('/', function (req, res) {
         res.send('<h1>V-Land Game Server</h1><p>Running...🏃‍♂️💨</p><a href="/colyseus">Colyseus Monitoring</a>')
     })
